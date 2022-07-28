02289af6ce984ae970be31c4bc3c00a284741b87
emmanuelsxe <errg07@gmail.com>
2022-07-27 20:27:29 -0500

    Seccion de operadores completa

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,14 @@
+const numero1 = 20;
+const numero2 = "20";
+const numero3 = 30;
+
+let numero; 
+console.log (numero); 
+console.log (typeof numero);
+
+let numeroDos = null;
+console.log (numeroDos);
+console.log (typeof numeroDos);
+
+console.log (numero == numeroDos);
+console.log (numero === numeroDos);
